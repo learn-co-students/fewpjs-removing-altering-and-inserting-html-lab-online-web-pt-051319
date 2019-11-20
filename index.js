9d675c1ec1ce0f2080fb62caaeae4181374640cd
ablakerose <bdecktor@gmail.com>
2019-11-20 11:14:45 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+document.body.removeChild(document.body.querySelector('#main'))
+
+let newHeader = document.createElement('h1');
+newHeader.id = "victory"
+newHeader.innerHTML = `Blake is the champion.`
+
