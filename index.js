71f7158d5bb3fdf91efbd7dee8969c175b9881c3
Jen <jenniferanndcb@gmail.com>
2019-11-23 21:15:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+
+let el = document.getElementById("main");
+
+el.remove();
+
+let newHeader = document.createElement("h1");
+
+document.body.appendChild(newHeader);
+
+newHeader.innerHTML = "YOUR-NAME is the champion"
+
+newHeader.id = "victory"
